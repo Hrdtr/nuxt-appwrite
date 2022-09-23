cf64cb43f8a4f6a64e014e130cc06a21d939360e
alix <alix@kronikle.eu>
2022-09-23 20:18:17 +0200

    🚧 updated API

--- src/runtime/sdk.js
+++ src/runtime/sdk.js
@@ -1,11 +1,23 @@
 import Appwrite from 'appwrite'
 
 export default (endpoint, project, jwt) => {
+  const client  = new Appwrite.Client()
+                        .setEndpoint(endpoint)
+                        .setProject(project)
+                        .setJWT(jwt)
   return {
     appwrite: Appwrite,
-    client: new Appwrite.Client()
-    .setEndpoint(endpoint)
-    .setProject(project)
-    .setJWT(jwt)
+    client: client,
+    account: new Appwrite.Account(client),
+    query: Appwrite.Query,
+    avatars: new Appwrite.Avatars(client),
+    database: new Appwrite.Databases(client),
+    functions: new Appwrite.Functions(client),
+    locale: new Appwrite.Locale(client),
+    storage: new Appwrite.Storage(client),
+    teams: new Appwrite.Teams(client),
+    permission: Appwrite.Permission,
+    role: Appwrite.Role,
+    ID: Appwrite.ID,
   }
 }
