280a03419aeacc6a4d1cc44a1a9afd2757165f1e
Herdi Tr <iam@icm.hrdtr.dev>
2024-01-11 11:46:28 +0700

    chore: add ci workflow

--- test/fixtures/basic/nuxt.config.ts
+++ test/fixtures/basic/nuxt.config.ts
@@ -1,5 +1,6 @@
-import { NuxtModule } from '@nuxt/schema'
-import nuxtAppwrite, { ModuleOptions } from '../../../src/module'
+import type { NuxtModule } from '@nuxt/schema'
+import type { ModuleOptions } from '../../../src/module'
+import nuxtAppwrite from '../../../src/module'
 
 export default defineNuxtConfig({
   modules: [nuxtAppwrite as NuxtModule<Partial<ModuleOptions>>],
